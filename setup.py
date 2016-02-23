24daddeef09d1c576c0d74c00c1b8d314c0ac2af
Sambit Mishra <sambitmishra@sambits-mbp.corp.moz.com dal.moz.com tuk.moz.com proc.dal.moz.com>
2016-02-23 11:33:29 -0800

    ROGER-783:[CLI]CLI updates to support separate CLI scripts

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='roger-cli',
+    name='roger-mesos-cli',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
