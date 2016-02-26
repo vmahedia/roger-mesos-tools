05da97bcf9eb4535ca1ce324d64736834a44ab89
ankan <ankan@moz.com>
2016-02-26 10:23:05 -0800

    Fixed some tests.

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -13,18 +13,18 @@ from settings import Settings
 class TestSettings(unittest.TestCase):
 
   def setUp(self):
-    pass
+    self.settingObj = Settings()
 
   def test_getConfigDir(self):
     set_config_dir = ''
     if "ROGER_CONFIG_DIR" in os.environ:
       set_config_dir = os.environ.get('ROGER_CONFIG_DIR')
     os.environ["ROGER_CONFIG_DIR"] = "/vagrant/testconfigdir"
-    config_dir = settingObj.getConfigDir()
+    config_dir = self.settingObj.getConfigDir()
     assert config_dir == "/vagrant/testconfigdir"
     del os.environ['ROGER_CONFIG_DIR']
     try:
-      config_dir = settingObj.getConfigDir()
+      config_dir = self.settingObj.getConfigDir()
     except (SystemExit) as e:
       assert ("Environment variable $ROGER_CONFIG_DIR is not set. Exiting." in e)
     if set_config_dir.strip() != '':
@@ -35,11 +35,11 @@ class TestSettings(unittest.TestCase):
     if "ROGER_COMPONENTS_DIR" in os.environ:
       set_comp_dir = os.environ.get('ROGER_COMPONENTS_DIR')
     os.environ["ROGER_COMPONENTS_DIR"] = "/vagrant/testcompdir"
-    comp_dir = settingObj.getComponentsDir()
+    comp_dir = self.settingObj.getComponentsDir()
     assert comp_dir == "/vagrant/testcompdir"
     del os.environ['ROGER_COMPONENTS_DIR']
     try:
-      comp_dir = settingObj.getComponentsDir()
+      comp_dir = self.settingObj.getComponentsDir()
     except (SystemExit) as e:
       assert ("Environment variable $ROGER_COMPONENTS_DIR is not set. Exiting." in e)
     if set_comp_dir.strip() != '':
@@ -50,11 +50,11 @@ class TestSettings(unittest.TestCase):
     if "ROGER_TEMPLATES_DIR" in os.environ:
       set_temp_dir = os.environ.get('ROGER_TEMPLATES_DIR')
     os.environ["ROGER_TEMPLATES_DIR"] = "/vagrant/testtempldir"
-    temp_dir = settingObj.getTemplatesDir()
+    temp_dir = self.settingObj.getTemplatesDir()
     assert temp_dir == "/vagrant/testtempldir"
     del os.environ['ROGER_TEMPLATES_DIR']
     try:
-      temp_dir = settingObj.getTemplatesDir()
+      temp_dir = self.settingObj.getTemplatesDir()
     except (SystemExit) as e:
       assert ("Environment variable $ROGER_TEMPLATES_DIR is not set.Exiting." in e)
     if set_temp_dir.strip() != '':
@@ -65,11 +65,11 @@ class TestSettings(unittest.TestCase):
     if "ROGER_SECRETS_DIR" in os.environ:
       set_sect_dir = os.environ.get('ROGER_SECRETS_DIR')
     os.environ["ROGER_SECRETS_DIR"] = "/vagrant/testsectdir"
-    sect_dir = settingObj.getSecretsDir()
+    sect_dir = self.settingObj.getSecretsDir()
     assert sect_dir == "/vagrant/testsectdir"
     del os.environ['ROGER_SECRETS_DIR']
     try:
-      sect_dir = settingObj.getSecretsDir()
+      sect_dir = self.settingObj.getSecretsDir()
     except (SystemExit) as e:
       assert ("Environment variable $ROGER_SECRETS_DIR is not set. Exiting." in e)
     if set_sect_dir.strip() != '':
@@ -77,12 +77,11 @@ class TestSettings(unittest.TestCase):
 
   def test_getCliDir(self):
     set_cli_dir = ''
-    cli_dir = settingObj.getCliDir()
+    cli_dir = self.settingObj.getCliDir()
     assert cli_dir is not None
 
   def tearDown(self):
     pass
 
 if __name__ == '__main__':
-  settingObj = Settings()
   unittest.main()
