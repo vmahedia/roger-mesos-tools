b76aa053f28fd1e8c70d82cce509e5f2443b979e
Manish Ranjan <cse1.manish@gmail.com>
2016-07-15 22:19:08 -0700

    ROGER-1080: PEP8 coding standards

--- cli/hooks.py
+++ cli/hooks.py
@@ -37,41 +37,55 @@ class Hooks:
             message = 'default message'
             envr = 'NA'
             temp = hook_input_metric.split(',')
-            action, app_name, config_name, envr, user = temp[0], temp[1], temp[3], temp[4], temp[5]
+            action, app_name, config_name, envr, user = (temp[0],
+                                                         temp[1], temp[3],
+                                                         temp[4], temp[5])
             action = ''.join(action.split('.')[1])
             action = '-'.join(action.split('_')[0:2])
             try:
                 channelsSet = Set(appdata['notifications']['channels'])
                 envSet = Set(appdata['notifications']['envs'])
                 commandsSet = Set(appdata['notifications']['commands'])
-                if len(channelsSet) == 0 or len(envSet) == 0 or len(commandsSet) == 0: # to handle no tag at all
-                    message = str('*Switching to defaults*: All environments, all actions')
+                if (len(channelsSet) == 0 or
+                        len(envSet) == 0 or
+                        len(commandsSet) == 0):  # to handle no tag at all
+                    message = str('*Switching to defaults*: \
+                                  All environments, all actions')
+                    # Default message  to slack channel just once
                     if self.flag is False:
-                        self.whobj.api_call(message,defChannel) # Default message  to slack channel just once
+                        self.whobj.api_call(message, defChannel)
                     channelsSet = [defChannel]
-                    envSet = ['dev', 'production', 'staging', 'local'] # default as e
-                    commandsSet = ['pull','build', 'push']
+                    envSet = ['dev', 'production', 'staging', 'local']
+                    commandsSet = ['pull', 'build', 'push']
                     self.flag = True
 
+                # to handle all tag for env and commands
                 if list(envSet)[0] == 'all':
-                    envSet = ['dev', 'production', 'staging', 'local'] # to handle all tag
+                    envSet = ['dev', 'production', 'staging', 'local']
 
                 if list(commandsSet)[0] == 'all':
-                    commandsSet = ['pull','build', 'push'] # to handle all tag
+                    commandsSet = ['pull', 'build', 'push']
 
             except (Exception) as e:
-                self.whobj.api_call("The following error occurred: %s" %e , defChannel)
+                # notify to channel and log it as well
+                self.whobj.api_call("The following error occurred: %s" %
+                                    e, defChannel)
                 print("The following error occurred: %s" %
                       e, file=sys.stderr)
                 raise
 
-            if ('post' in action and envr.split('=')[1] in envSet and action.split('-')[1] in commandsSet):
+            if ('post' in action and envr.split('=')[1] in envSet and
+                    action.split('-')[1] in commandsSet):
                 for channel in channelsSet:
-                    slackMessage = ("Completed *" +action.split('-')[1] +"* of *" +app_name.split('=')[1]+
-                    "* on *"+envr.split('=')[1]+ "* in *" +
-                    str((datetime.now() - function_execution_start_time).total_seconds()*1000) +
-                    "* miliseconds (triggered by *"+ user.split('=')[1] +"*)")
-                    self.whobj.api_call(slackMessage,'#'+channel)
+                    slackMessage = ("Completed *" + action.split('-')[1] +
+                                    "* of *" + app_name.split('=')[1] +
+                                    "* on *" + envr.split('=')[1] + "* in *" +
+                                    str((datetime.now() -
+                                         function_execution_start_time)
+                                        .total_seconds()*1000) +
+                                    "* miliseconds (triggered by *" +
+                                    user.split('=')[1] + "*)")
+                    self.whobj.api_call(slackMessage, '#' + channel)
             abs_path = os.path.abspath(path)
             if "hooks" in appdata and hookname in appdata["hooks"]:
                 command = appdata["hooks"][hookname]
@@ -83,17 +97,23 @@ class Hooks:
             print("The following error occurred : %s" %
                   e, file=sys.stderr)
             execution_result = 'FAILURE'
-            self.whobj.api_call("The following error occurred : %s" %e ,defChannel)
+            self.whobj.api_call("The following error occurred: %s" %
+                                e, defChannel)
             raise
         finally:
             try:
-                if 'execution_result' not in globals() or 'execution_result' not in locals():
+                if ('execution_result' not in globals() or
+                        'execution_result' not in locals()):
                     execution_result = 'FAILURE'
-                if 'function_execution_start_time' not in globals() or 'function_execution_start_time' not in locals():
+                if ('function_execution_start_time' not in globals() or
+                        'function_execution_start_time' not in locals()):
                     function_execution_start_time = datetime.now()
                 sc = self.utils.getStatsClient()
-                time_take_milliseonds = ((datetime.now() - function_execution_start_time).total_seconds() * 1000)
-                hook_input_metric = hook_input_metric + ",outcome=" + str(execution_result)
+                time_take_milliseonds = ((datetime.now() -
+                                          function_execution_start_time).
+                                         total_seconds() * 1000)
+                hook_input_metric = (hook_input_metric + ",outcome=" +
+                                     str(execution_result))
                 sc.timing(hook_input_metric, time_take_milliseonds)
             except (Exception) as e:
                 print("The following error occurred: %s" %
