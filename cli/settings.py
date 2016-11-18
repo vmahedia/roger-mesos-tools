b59cbf1b3b91debb6e80ba0a702e375df728ede0
Demitri Swan <demitri@moz.com>
2016-11-18 10:04:47 -0800

    Done with settings.py

--- cli/settings.py
+++ cli/settings.py
@@ -9,6 +9,11 @@ import getpass
 class Settings:
 
     def getConfigDir(self):
+        """
+        Return the config dir from ROGER_CONFIG_DIR if not empty
+
+        return: [String] Path to the config dir
+        """
         config_dir = ''
         if "ROGER_CONFIG_DIR" in os.environ:
             config_dir = os.environ.get('ROGER_CONFIG_DIR')
@@ -19,6 +24,9 @@ class Settings:
         return config_dir
 
     def getComponentsDir(self):
+        """
+        Return the components dir from ROGER_COMPONENTS_DIR if not empty
+        """
         comp_dir = ''
         if "ROGER_COMPONENTS_DIR" in os.environ:
             comp_dir = os.environ.get('ROGER_COMPONENTS_DIR')
@@ -29,6 +37,9 @@ class Settings:
         return comp_dir
 
     def getTemplatesDir(self):
+        """
+        Return the templates dir from ROGER_TEMPLATES_DIR if not empty
+        """
         templ_dir = ''
         if "ROGER_TEMPLATES_DIR" in os.environ:
             templ_dir = os.environ.get('ROGER_TEMPLATES_DIR')
@@ -38,6 +49,9 @@ class Settings:
         return templ_dir
 
     def getSecretsDir(self):
+        """
+        Return the absolute path to the secrets dir from ROGER_SECRETS_DIR
+        """
         secrets_dir = ''
         if "ROGER_SECRETS_DIR" in os.environ:
             secrets_dir = os.environ.get('ROGER_SECRETS_DIR')
@@ -48,12 +62,21 @@ class Settings:
         return secrets_dir
 
     def getCliDir(self):
+        """
+        Return the absolute, non-linked path to the cli directory
+        """
         cli_dir = ''
         own_dir = os.path.dirname(os.path.realpath(__file__))
         cli_dir = os.path.abspath(os.path.join(own_dir, os.pardir))
         return cli_dir
 
     def getUser(self):
+        """
+        Return the roger user
+
+        If ROGER_USER is set, return it. Otherwise, as the user at the prompt
+        and return the value from the interactive session.
+        """
         user = None
         # ROGER_USER_ID env var > getpass.getuser()
         envvar = "ROGER_USER"
@@ -64,6 +87,15 @@ class Settings:
         return user
 
     def getPass(self, env):
+        """
+        Return the user password
+
+        env: [String] The desired environment
+
+        Return the password for the given user and environment from the env var
+        ROGER_USER_PASS_$ENV. Otherwise, return the value provided from the
+        prompt
+        """
         # ROGER_USER_PASS_<ENV> > getpass.getpass()
         envvar = "ROGER_USER_PASS_{}".format(env.upper())
         if envvar in os.environ:
