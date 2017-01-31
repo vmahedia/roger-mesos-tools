3dc7ab7e04ccc9fb2676d22aeaf9eeb768714230
Sarah Kane <sarah.kane@moz.com>
2017-01-31 14:21:02 -0800

    fixed test_image_name test

--- tests/unit/test_roger_promote.py
+++ tests/unit/test_roger_promote.py
@@ -41,92 +41,70 @@ class TestRogerPromote(unittest.TestCase):
         return os.environ['ROGER_CONFIG_DIR']
 
     def test_config_dir(self):
-        # Instance
+
         rp = RogerPromote()
         assert rp.config_dir == '/vagrant/config'
 
     def test_roger_env(self):
-        # Fakes
+
         fake_config = tests.helper.fake_config()
         settings = mock(Settings)
 
-        # Stubs
         when(self.app_config).getRogerEnv(
             self.config_dir
         ).thenReturn(fake_config)
 
-        # Instance
         rp = RogerPromote(app_config=self.app_config)
         assert rp.roger_env == fake_config
 
     def test_set_framework(self):
-        # app_data is a dict taken from the config file for a given app
         app_data = {'test_app': {'name': 'test_app'}}
 
-
-        # Stubs
         when(self.app_config).getAppData(
             self.config_dir, self.config_file, 'test_app'
         ).thenReturn(app_data)
 
-        # Instance
         rp = RogerPromote(app_config=self.app_config)
 
-        # Call the method to set the framework
         rp._set_framework(self.config_file, 'test_app')
-        # Get the framework name by calling getName() on the object. It should
-        # default to Marathon since we didn't set the framework key in the data
-        # var
+
         assert rp._framework.getName() == 'Marathon'
 
     def test_image_name(self):
-
         os.environ['ROGER_USER'] = "first.last"
         os.environ['ROGER_USER_PASS_DEV'] = "password"
         os.environ['ROGER_USER_PASS_STAGE'] = "password"
         os.environ['ROGER_USER_PASS_PROD'] = "password"
 
-        # Fakes
         framework = mock(Marathon)
-        app_config = mock(AppConfig)
-        settings = mock(Settings)
-        #fake_config = tests.helper.fake_config()
 
         when(framework).getName().thenReturn("Marathon")
-        when(framework).image_name('first.last', "password", "dev", "app_id", self.config_dir, self.config_file).thenReturn("test_image")
-        when(framework).app_id("test_path/test_app.json", "Marathon").thenReturn("app_id")
+        when(framework).app_id(
+            "test_path/test_app.json",
+            "Marathon"
+        ).thenReturn("app_id")
+        when(framework).image_name(
+            'first.last',
+            "password",
+            "dev",
+            "app_id",
+            self.config_dir,
+            self.config_file
+        ).thenReturn("test_image")
 
-        rp = RogerPromote(
-            app_config=app_config,
-            framework=framework
-        )
-        assert rp._image_name('dev', self.config_file, "test_path/test_app.json") == 'test_image'
-
-        os.environ['ROGER_USER'] = 'test_user'
-        os.environ['ROGER_USER_PASS_DEV'] = 'test_user.test_user'
-
-        when(self.framework).app_id('/temp/file').thenReturn("test_app")
-
-        when(self.framework).image_name(
-            os.environ['ROGER_USER'],
-            os.environ['ROGER_USER_PASS_DEV'],
-            'dev', 'test_app',
-            os.environ['ROGER_CONFIG_DIR'],
-            'config_file').thenReturn("test_image")
-
-        rp = RogerPromote(framework=self.framework)
-        assert rp._image_name('dev', 'config_file', '/temp/file') == 'test_image'
-
+        rp = RogerPromote(framework=framework)
+        assert rp._image_name(
+            'dev',
+            self.config_file,
+            "test_path/test_app.json") == 'test_image'
 
     def test_config_resolver(self):
-        # Fakes
         framework = mock(Framework)
         settings = mock(Settings)
         app_config = mock(AppConfig)
         config_dir = '/vagrant/config'
         fake_team_config = tests.helper.fake_team_config()
 
-        # Stubs
         when(settings).getConfigDir().thenReturn(config_dir)
         when(app_config).getConfig(
             config_dir, 'roger.json'
