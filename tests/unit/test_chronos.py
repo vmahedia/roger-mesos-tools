0a14411939166e9d04a5647b75e1dc014ea18f4b
Sarah Kane <sarah.kane@moz.com>
2017-02-02 13:11:17 -0800

    changed FDQN to an example FDQN in test_chronos.py

--- tests/unit/test_chronos.py
+++ tests/unit/test_chronos.py
@@ -22,9 +22,9 @@ class TestChronos(unittest.TestCase):
 
     def test_image_name(self):
         res = mock(requests.Response)
-        url = 'https://chronos-dev-roger.dal.moz.com/scheduler/jobs/search?name=application-platforms-app1'
-        image_data = [{'container':{'image':'moz-roger-simpleapp-1abd568915ad7598a37d1564b103b5b3dc8d9507/v0.1.0'}}]
-        data = {'environments':{'dev':{'chronos_endpoint':'https://chronos-dev-roger.dal.moz.com'}}}
+        url = 'https://chronos-dev-env.place.testplace.com/scheduler/jobs/search?name=example-team'
+        image_data = [{'container':{'image':'example-image/v0.1.0'}}]
+        data = {'environments':{'dev':{'chronos_endpoint':'https://example-end-point.com'}}}
         os.environ['ROGER_CONFIG_DIR'] = '/vagrant/config'
         username = 'first.first'
         password = 'last.last'
